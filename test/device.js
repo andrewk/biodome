4ec8eb368c5297ed5aaac046cc6373d6ddb9ad04
andrewk <andrew@leftjustified.net>
2013-11-26 09:17:14 +1100

    Device can be turned on or off

--- test/device.js
+++ test/device.js
@@ -5,12 +5,7 @@ var chai = require("chai")
   , gpio = require("./mocks/gpio");
 
 describe('Device', function() {
-  describe('initialize()', function() {
-    it('should be in the init state when first created', function(){
-      var device = new Device(gpio.export(1));
-      expect(device.is("init")).to.be.true;
-    });
-
+  describe('#initialize', function() {
     it('should have a gpio pin', function(){
       var device = new Device(gpio.export(2));
       expect(device.gpio).to.be.ok;
@@ -20,6 +15,45 @@ describe('Device', function() {
       var device = new Device(gpio.export(1, {"direction":"in"}));
       expect(device.gpio.direction).to.equal("out");
     });
-  })
+
+    it('should have an id', function() {
+      var d = new Device(gpio.export(1), "switch");
+      expect(d.id).to.equal("switch");
+    });
+
+    it('should be in the off state when first created', function(){
+      var device = new Device(gpio.export(1));
+      expect(device.is("off")).to.be.true;
+    });
+  });
+
+  describe('#on', function() {
+    it('should set device state to "on"', function() {
+      var d = new Device(gpio.export(1));
+      d.on();
+      expect(d.is("on")).to.be.true;
+    });
+
+    it('should set GPIO value to 1', function() {
+      var d = new Device(gpio.export(1));
+      d.on();
+      expect(d.gpio.value).to.equal(1);
+    });
+  });
+
+  describe('#off', function() {
+
+    it('should set device state to "off"', function() {
+      var d = new Device(gpio.export(1));
+      d.off();
+      expect(d.is("off")).to.be.true;
+    });
+
+    it('should set GPIO value to 0', function() {
+      var d = new Device(gpio.export(1));
+      d.off();
+      expect(d.gpio.value).to.equal(0);
+    });
+  });
 });
 
