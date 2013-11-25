0dccd3f476cf0d0bb5718104bfce36d5ea8cf0ca
andrewk <andrew@leftjustified.net>
2013-11-26 08:44:21 +1100

    Device requires GPIO pin export

--- test/device.js
+++ test/device.js
@@ -1,14 +1,25 @@
 var chai = require("chai")
   , sinon = require("sinon")
   , expect = chai.expect
-  , Device = require("../lib/device");
+  , Device = require("../lib/device")
+  , gpio = require("./mocks/gpio");
 
 describe('Device', function() {
   describe('initialize()', function() {
     it('should be in the init state when first created', function(){
-      var device = new Device();
+      var device = new Device(gpio.export(1));
       expect(device.is("init")).to.be.true;
-    })
+    });
+
+    it('should have a gpio pin', function(){
+      var device = new Device(gpio.export(2));
+      expect(device.gpio).to.be.ok;
+    });
+
+    it('ensures its GPIO has the direction set to out', function(){
+      var device = new Device(gpio.export(1, {"direction":"in"}));
+      expect(device.gpio.direction).to.equal("out");
+    });
   })
 });
 
