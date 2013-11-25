8dff402d76cff0fc5687bb5e160d90f4766ffa88
Andrew K <andrew@leftjustified.net>
2013-11-25 11:29:49 +1100

    use chai assertion library. Fix Device exports

--- test/device.js
+++ test/device.js
@@ -1,11 +1,16 @@
-var assert = require("assert");
-var expect = require("expect");
+var chai = require("chai")
+  , spies = require("chai-spies");
+
+chai.use(spies);
+
+var expect = chai.expect;
+
 var Device = require("../lib/device");
 
 describe('Device', function(){
   describe('initialize()', function(){
     it('should be in the init state when first created', function(){
-      var device = new Device;
+      var device = new Device();
       expect(device.is("init")).to.be.true;
     })
   })
