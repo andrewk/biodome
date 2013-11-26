9e6944333cc7d3b3900dcd23aee5182ca5e4307a
andrewk <andrew@leftjustified.net>
2013-11-26 19:15:46 +1100

    formatting error in spec

--- test/app.js
+++ test/app.js
@@ -4,16 +4,21 @@ var chai = require("chai")
 
 var A = require('../lib/app.js')
   , nconf = require('nconf');
+describe("App", function() {
+  describe('#new', function() {
+    it('uses mock GPIO in development', function() {
+      var app = new A();
+      expect(app.gpio).to.deep.equal(require('./mocks/gpio'));
+    });
 
-describe('#new', function() {
-  it('uses mock GPIO in development', function() {
-    var app = new A();
-    expect(app.gpio).to.deep.equal(require('./mocks/gpio'));
+    it('uses hardware GPIO in production', function() {
+      nconf.overrides({"NODE_ENV" : "production"});
+      var app = new A();
+      expect(app.gpio).to.deep.equal(require('./mocks/gpio'));
+    });
   });
 
-  it('uses hardware GPIO in production', function() {
-    nconf.overrides({"NODE_ENV" : "production"});
-    var app = new A();
-    expect(app.gpio).to.deep.equal(require('./mocks/gpio'));
+  describe('.devices', function() {
+    it('contains an array of device instances');
   });
 });
