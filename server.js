4aea850146898a871ab9fd0c3cb690946bbb359a
andrewk <andrew@leftjustified.net>
2013-11-28 17:56:42 +1100

    Update demo app

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
-var restify = require('restify')
-  , config = require('./config/app.js')
-  , routes = require('./app/routes.js')
-  , serverApp = require('./app/server-app.js')
+var config = require('./config/app.js')
+  , biodome = require('./app/app.js')
   , device = require('./app/device.js');
 
-var app = new serverApp(config);
+var app = new biodome(config);
 
 app.devices.push(new device(app.gpio.export(1), "exhaust"));
 app.devices.push(new device(app.gpio.export(2), "irrigation"));
 
-server = routes(app);
-server.listen(8080);
+app.server().listen(config.get('PORT'));
