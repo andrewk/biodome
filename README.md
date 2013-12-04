bce9db56e93086d38924bd7c51d754b2b71aaea4
andrewk <andrew@leftjustified.net>
2013-12-04 14:26:05 +1100

    Travis CI

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 biodome-server
 ==============
 
-Node.js server for Device/Sensor network running on a Raspberry Pi
+[![Build Status](https://secure.travis-ci.org/andrewk/biodome-server.png?branch=master)](http://travis-ci.org/andrewk/biodome-server)
 
+Node.js server for Device/Sensor network running on a Raspberry Pi
 
 API
 ===
 ```
 GET /devices
 GET /devices/:id
-PUT /devices/:id 
+PUT /devices/:id
 
 GET /sensors
 GET /sensors/:id
--- test/blueprints/driver.js
+++ test/blueprints/driver.js
@@ -1,18 +1,14 @@
+var Base = require('../../lib/drivers/base')
+
 var DummyDriver = function() {
-  var self = this;
-  self.value = null;
-
-  self.updateSensor = function(sensor, callback) {
-    sensor.value = self.read();
-    sensor.markComplete();
-    if("function" == typeof callback)
-      callback(null, sensor);
-  }
-
-  self.read = function(callback) {
-    self.value = Math.floor(Math.random() * 100);
+  this.read = function(callback) {
+    error = null;
+    this.value = Math.floor(Math.random() * 100);
+    callback(error);
   }
 };
+
+DummyDriver.prototype = new Base;
 
 module.exports = DummyDriver;
 module.exports.make = function() { return new DummyDriver() };
