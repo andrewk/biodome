fdb10df084e3e9f381e02fd922cfc9c9d55ffa93
Andrew Krespanis <andrew@leftjustified.net>
2014-01-11 22:11:26 +1100

    Reorganize package, add index

---
+++ index.js
@@ -0,0 +1,17 @@
+// load all the things
+
+require('fs').readdirSync(__dirname + '/lib/').forEach(function(file) {
+  if (file.match(/.+\.js/g) !== null && file !== 'index.js') {
+    var name = file.replace('.js', '');
+    exports[name] = require('./lib/' + file);
+  }
+});
+
+exports.io = {};
+require('fs').readdirSync(__dirname + '/lib/io/').forEach(function(file) {
+  if (file.match(/.+\.js/g) !== null && file !== 'index.js') {
+    var name = file.replace('.js', '');
+    exports.io[name] = require('./lib/io/' + file);
+  }
+});
+
