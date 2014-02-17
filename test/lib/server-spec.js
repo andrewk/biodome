47031222d4bf7cf00fe1b7953b1533333c6c6dbd
Andrew Krespanis <andrew@leftjustified.net>
2014-02-18 09:16:44 +1100

    Server inherits http(s).Server

--- test/lib/server-spec.js
+++ test/lib/server-spec.js
@@ -1,38 +1,52 @@
 var chai = require("chai")
   , sinon = require("sinon")
+  , request = require("supertest")
   , expect = chai.expect
-  , server = require('../../lib/server').factory
+  , serverFactory = require('../../lib/server').factory
+  , app = require('../blueprints/app');
 
 describe('connection', function() {
-  before(function() {
-    s = server();
-  });
-
-  after(function() {
-    s.close();
-  });
-
   it('refuses access without correct access token');
   it('confirms connection with correct access token');
+  it('sends system status data on successful connection');
 });
 
 describe('SSL connection', function() {
-  before(function() {
-    s = server();
-  });
-
-  after(function() {
-    s.close();
-  });
-
   it('provides an SSL certificate');
 });
 
+describe('server status', function() {
+  before(function() {
+    server = serverFactory(app.make());
+  });
+
+  it('responds with JSON server uptime and system stats', function(done) {
+    request(server)
+      .get('/status')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) throw err;
+        server.close();
+        done();
+      }); 
+  });
+});
+
 describe('client message received', function() {
-  it('passes the event to its app');
+  it.skip('validates the message', function(done) {
+    var validator = sinon.spy();
+    var msgGateway = sinon.spy();
+    s.msgHandler = { 
+      validateMessage : validator,
+      sendMessageToApp : msgGateway
+    };
+    done(new Error('fail')); 
+  });
 });
 
 describe('app events', function() {
   it('broadcasts sensor update');
   it('broadcasts device update');
 });
+
