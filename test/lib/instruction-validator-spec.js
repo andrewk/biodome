27ede3fe1862ede894f766ccf86ab980500cb7ac
Andrew Krespanis <andrew@leftjustified.net>
2014-05-20 21:12:16 +1000

    Remove support for multi-instruction app messages

--- test/lib/instruction-validator-spec.js
+++ test/lib/instruction-validator-spec.js
@@ -7,77 +7,41 @@ var result;
 
 describe('InstructionValidator', function() {
   describe('#validateMessage', function() {
-    it('errors on empty message', function() {
-      result = validator.validateSet('');
-      assert(!result.valid, 'expect result to be invalid');
-      expect(result.error).to.equal('Empty instruction set provided');
-    });
-
-    it('errors on incorrectly-formatted message', function() {
-      result = validator.validateSet({bllasddr:"ASda"});
-      expect(result.error).to.equal('Instruction set must be an array');
-      assert(!result.valid);
-    });
-
     it('errors on missing selector', function() {
-      msg = [
-        {
-          'selector' : {'id':'foo'},
-          'command' : {'type' : 'write', 'value':  1234}
-        },
-        {
-          'squirrels' : {'id':'foo'},
-          'command' : {'type' : 'write', 'value':  1234}
-        }
-      ];
-      result = validator.validateSet(msg);
+      msg = {
+        'squirrels' : {'id':'foo'},
+        'command' : {'type' : 'write', 'value':  1234}
+      };
+      result = validator.validate(msg);
       assert(!result.valid, 'expect invalid result');
-      expect(result.error).to.equal('Invalid instruction: Missing selector in instruction 2');
+      expect(result.error).to.equal('Invalid instruction: Missing selector');
     });
 
     it('errors on missing command', function() {
-      msg = [
-        {
-          'selector' : {'id':'foo'},
-          'command' : {'type' : 'write', 'value':  1234}
-        },
-        {
-          'selector' : {'id':'foo'},
-        }
-      ];
-      result = validator.validateSet(msg);
+      msg = { 'selector' : {'id':'foo'} };
+      result = validator.validate(msg);
       assert(!result.valid, 'expect invalid result');
-      expect(result.error).to.equal('Invalid instruction: Missing command in instruction 2');
+      expect(result.error).to.equal('Invalid instruction: Missing command');
     });
 
     it('errors on missing command type', function() {
-      msg = [
-        {
-          'selector' : {'id':'foo'},
-          'command' : {'type' : 'write', 'value':  1234}
-        },
-        {
-          'selector' : {'id':'foo'},
-          'command' : {'value':  1234}
-        }
-      ];
-      result = validator.validateSet(msg);
+      msg = {
+        'selector' : {'id':'foo'},
+        'command' : {'value':  1234}
+      };
+
+      result = validator.validate(msg);
       assert(!result.valid, 'expect invalid result');
-      expect(result.error).to.equal('Invalid instruction: Invalid command in instruction 2');
+      expect(result.error).to.equal('Invalid instruction: Invalid command');
     });
 
-    it('passes on a valid instruction set', function() {
-      var msg = [
-        {
-          'selector' : {'id':'foo'},
-          'command' : {'type' : 'write', 'value':  1234}
-        },
-        {
-          'selector' : {'id':'foo'},
-          'command' : {'type': 'write', 'value':  1234}
-        }
-      ];
-      result = validator.validateSet(msg);
+    it('passes on a valid instruction', function() {
+      var msg = {
+        'selector' : {'id':'foo'},
+        'command' : {'type' : 'write', 'value':  1234}
+      };
+       
+      result = validator.validate(msg);
       assert(result.valid, 'expect valid result');
       assert(result.error == null);
     });
