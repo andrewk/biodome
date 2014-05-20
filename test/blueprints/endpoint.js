bac5b5bb8d79ce8298c90c072343fb6f95672f1a
Andrew Krespanis <andrew@leftjustified.net>
2014-05-20 21:11:53 +1000

    Fix bug whereby all blueprint Endpoints shared a driver

--- test/blueprints/endpoint.js
+++ test/blueprints/endpoint.js
@@ -1,6 +1,10 @@
-var endpoint = require('../../lib/endpoint')
-  , driver = require('../../lib/drivers/base')
-  , io = require('../mocks/io').new();
+var endpoint = require('../../lib/endpoint');
+
+function driver() {
+  return require("../../lib/drivers/base").new(
+    require('../mocks/io').new(true)
+  );
+}
 
 module.exports = endpoint;
 
@@ -8,7 +12,7 @@ module.exports.make = function(props) {
   props = props || {};
   return new endpoint({
     'type': props.type || 'blergh',
-    'id'  : props.id || 'endpoint_' + Math.floor(Math.random() * 100),
-    'driver': props.driver || new driver(io)
+    'id'  : props.id || 'endpoint_' + Math.floor(Math.random() * 1000),
+    'driver': props.driver || driver()
   });
 };
