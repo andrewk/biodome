483a5566c1547a8d1fe0fd3918d9dddb557c443b
Andrew Krespanis <andrew@leftjustified.net>
2015-06-30 19:55:39 +1000

    Scrap nonsense package entrypoint

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
-// load all the things to provide Biodome convenience loader
+// FIXME: rewrite using ES6 module export synt
 
-var fs = require('fs');
-
-fs.readdirSync(__dirname + '/lib/').forEach(function(file) {
-  if (file.match(/.+\.js/g) !== null && file !== 'index.js') {
-    var name = file.replace('.js', '');
-    exports[name] = require('./lib/' + file).new;
-  }
-});
-
-exports.drivers = importDir('/lib/drivers/');
-exports.io = importDir('/lib/io/');
-
-function importDir(dir) {
-  var obj = {};
-  fs.readdirSync(__dirname + dir).forEach(function(file) {
-    if (file.match(/.+\.js/g) !== null) {
-      var name = file.replace('.js', '');
-      obj[name] = require('.' + dir + file).new;
-    }
-  });
-  return obj;
-}
+module.exports = {
+  Server: require('./lib/Server'),
+  Endpoint: require('./lib/Endpoint'),
+};
